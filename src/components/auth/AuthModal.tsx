d73ab15b8c87c8e83321d949cc85a7c2efe91a00
Brooke <bsextion@gmail.com>
2025-02-24 22:02:38 -0600

    adding form

--- src/components/auth/AuthModal.tsx
+++ src/components/auth/AuthModal.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import { Outlet, Navigate } from "react-router-dom";
+import { z } from "zod"
+ 
 const AuthModal = () => {
-  return (
-    <div>AuthModal</div>
-  )
-}
+  const isAuthenticated = false; // Replace with your authentication logic
 
-export default AuthModal
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" /> // Redirect to home if authenticated
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+          <img src="/public/assets/images/side-img.svg" alt="logo"
+          className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"/>
+        </>
+      )}
+    </>
+  );
+};
+
+export default AuthModal;
---
+++ src/lib/validation/index.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+//Define the validation schema for the register form.
+export const RegisterValidation = z.object({
+    name: z.string().min(2, { message: "Name must be at least 2 characters." }),
+    username: z.string().min(2, { message: "Username must be at least 2 characters." }),
+    email: z.string().email(),
+    password: z.string().min(8, { message: "Password must be at least 8 characters." }),
+});
