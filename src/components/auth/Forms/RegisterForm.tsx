2a1c8617dc0308bece1ec487ee5b48dbc57d13fa
Brooke <bsextion@gmail.com>
2025-02-25 23:33:59 -0600

    add createuser

--- src/components/auth/Forms/RegisterForm.tsx
+++ src/components/auth/Forms/RegisterForm.tsx
@@ -16,9 +16,11 @@ import { RegisterValidation } from "@/lib/validation";
 import { z } from "zod";
 import Loader from "@/components/common/Loader";
 import { Link } from "react-router-dom";
+import { createUserAccount } from "@/lib/appwrite/api";
 
 const RegisterForm = () => {
   const isLoading = false;
+
   // Define form.
   const form = useForm<z.infer<typeof RegisterValidation>>({
     resolver: zodResolver(RegisterValidation),
@@ -30,102 +32,106 @@ const RegisterForm = () => {
     },
   });
 
-  async function onSubmit(values: z.infer<typeof RegisterValidation>) {
-  //create user
+  const onSubmit = async (values: z.infer<typeof RegisterValidation>) => {
+    const newUser = await createUserAccount(values);
+    console.log('new user created: ', newUser);
+
   }
 
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create account. </h2>
+        <img src="/assets/images/logo.svg" alt="logo" />
+
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+          Create a new account
+        </h2>
         <p className="text-light-3 small-medium md:base-regular mt-2">
-          Please enter your account details.
+          To use snapgram, Please enter your details
         </p>
+
         <form
           onSubmit={form.handleSubmit(onSubmit)}
-          className="flex flex-col gap-5 w-full mt-4"
-        >
+          className="flex flex-col gap-5 w-full mt-4">
           <FormField
             control={form.control}
             name="name"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Name</FormLabel>
+                <FormLabel className="shad-form_label">Name</FormLabel>
                 <FormControl>
-                  <Input
-                    className="shad-input"
-                    type="text"
-                    placeholder="Enter first name"
-                  />
+                  <Input type="text" className="shad-input" {...field} />
                 </FormControl>
+                <FormMessage />
               </FormItem>
             )}
           />
+
           <FormField
             control={form.control}
             name="username"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Username</FormLabel>
+                <FormLabel className="shad-form_label">Username</FormLabel>
                 <FormControl>
-                  <Input
-                    className="shad-input"
-                    type="text"
-                    placeholder="Enter username"
-                  />
+                  <Input type="text" className="shad-input" {...field} />
                 </FormControl>
+                <FormMessage />
               </FormItem>
             )}
           />
+
           <FormField
             control={form.control}
             name="email"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Email</FormLabel>
+                <FormLabel className="shad-form_label">Email</FormLabel>
                 <FormControl>
-                  <Input
-                    className="shad-input"
-                    type="email"
-                    placeholder="Enter email address"
-                  />
+                  <Input type="text" className="shad-input" {...field} />
                 </FormControl>
+                <FormMessage />
               </FormItem>
             )}
           />
+
           <FormField
             control={form.control}
             name="password"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Password</FormLabel>
+                <FormLabel className="shad-form_label">Password</FormLabel>
                 <FormControl>
-                  <Input
-                    className="shad-input"
-                    type="password"
-                    placeholder="Enter password"
-                  />
+                  <Input type="password" className="shad-input" {...field} />
                 </FormControl>
+                <FormMessage />
               </FormItem>
             )}
           />
+
           <Button type="submit" className="shad-button_primary">
             {isLoading ? (
               <div className="flex-center gap-2">
                 <Loader /> Loading...
               </div>
             ) : (
-              "Register"
+              "Sign Up"
             )}
           </Button>
+
           <p className="text-small-regular text-light-2 text-center mt-2">
             Already have an account?
-            <Link to="/login" className="text-primary-500 text-small-semibold ml-1">Log in</Link>
+            <Link
+              to="/sign-in"
+              className="text-primary-500 text-small-semibold ml-1">
+              Log in
+            </Link>
           </p>
         </form>
       </div>
     </Form>
   );
 };
+
 
 export default RegisterForm;
